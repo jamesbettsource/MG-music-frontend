95eea8eaf5c5510e2b6749d1bc2480bfaced0a5d
James Kiprotich <james.bett@riarauniversity.ac.ke>
2026-02-15 16:52:19 +0300

    Fix routing with react-router and register page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
 
 
 // If you want to start measuring performance in your app, pass a function
